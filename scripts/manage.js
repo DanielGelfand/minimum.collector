97843c943cf166fac92ab2f2ab545eff8af12b2e
danielgelfand <>
2021-11-30 21:04:31 -0500

    template for checking if funds need moving

--- scripts/manage.js
+++ scripts/manage.js
@@ -2,3 +2,25 @@ const ethers = require("ethers");
 const {bondOrStake} = require("../utils/bondOrStake.js")
 const { strats } = require('../data/strats.js');
 
+const run = async () => {
+
+    for (let i = 0; i < strats.length; i++) {
+        strat = strats[i];
+        checkOptimal = await bondOrStake(strat);
+        
+        if (checkOptimal) {
+            console.log(checkOptimal);
+            console.log("Checking if bond available...")
+            // Get SpartacusStrategy Contract and check if bonded variable is True
+            // If False, unstake funds and bond
+        }
+        else {
+            console.log("Continue staking...");
+        }
+
+    }
+}
+
+(async () => {
+    await run();
+})();
