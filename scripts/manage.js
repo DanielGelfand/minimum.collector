3f1eb672acf5941b53590fdce940a3ab9ac61366
danielgelfand <>
2022-01-11 20:58:38 -0500

    check if time to trigger rebase

--- scripts/manage.js
+++ scripts/manage.js
@@ -1,11 +1,28 @@
-const ethers = require("ethers");
+const {ethers} = require("ethers");
 const {bondOrStake} = require("../utils/bondOrStake.js")
-const { strats } = require('../data/strats.js');
+const {strats} = require('../data/strats.js');
+const {canRebase} = require("../utils/checkRebase.js");
+
+const provider = ethers.getDefaultProvider("https://rpc.ftm.tools/");
+
 
 const run = async () => {
 
     for (let i = 0; i < strats.length; i++) {
         strat = strats[i];
+
+        let stakingAddress = strat["stakingContract"];
+        let stakingABI = strat["stakingContractABI"].abi;
+        let stakingContract = new ethers.Contract(stakingAddress, stakingABI, provider);
+
+        let currBlockNum = await provider.getBlockNumber()
+        if (await canRebase(currBlockNum, stakingContract)) {
+            console.log("Can rebase!");
+        }
+        else {
+            console.log("Cannot rebase");
+        }
+
         checkOptimal = await bondOrStake(strat);
         
         if (checkOptimal) {
