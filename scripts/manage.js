6d72fc9e6a7b6009844566c3c8f8160d585e6db0
danielgelfand <>
2022-01-16 17:51:00 -0500

    add check for 4,4

--- scripts/manage.js
+++ scripts/manage.js
@@ -1,26 +1,36 @@
 const {ethers} = require("ethers");
 const {bondOrStake} = require("../utils/bondOrStake.js")
 const {strats} = require('../data/strats.js');
-const {canRebase} = require("../utils/checkRebase.js");
+const {canRedeem} = require("../utils/checkRebase.js");
 
 const provider = ethers.getDefaultProvider("https://rpc.ftm.tools/");
 
-
+// Function to be called repeatedly to check if bond can be redeemed and
+// funds moved from staking to bonding
 const run = async () => {
+
+    let isBonding = true; // modify later to check in smart contract
 
     for (let i = 0; i < strats.length; i++) {
         strat = strats[i];
+        let is44 = strat["is44"];
 
         let stakingAddress = strat["stakingContract"];
         let stakingABI = strat["stakingContractABI"].abi;
+
+        let bondAddress = "0x5D449738986ab34280373502031D1513581Cb649"; // read currentBond
+        let bondABI = grabBondWithAddress(strat,bondAddress)["bondABI"].abi;
+
         let stakingContract = new ethers.Contract(stakingAddress, stakingABI, provider);
+        let bondContract = new ethers.Contract(bondAddress, bondABI, provider);
 
         let currBlockNum = await provider.getBlockNumber()
-        if (await canRebase(currBlockNum, stakingContract)) {
-            console.log("Can rebase!");
+        if (isBonding && await canRedeem(currBlockNum, stakingContract, bondContract, is44)) {
+            console.log("Can redeem bond!");
+            // redeem bond here
         }
         else {
-            console.log("Cannot rebase");
+            console.log("Cannot redeem bond");
         }
 
         checkOptimal = await bondOrStake(strat);
@@ -29,13 +39,23 @@ const run = async () => {
             console.log(checkOptimal);
             console.log("Checking if bond available...")
             // Get SpartacusStrategy Contract and check if bonded variable is True
-            // If False, unstake funds and bond
+            // If False, stakeToBond
         }
         else {
             console.log("Continue staking...");
         }
 
     }
+}
+
+// Grabs the bond from a strategy object with the given address
+const grabBondWithAddress = (strat, bondAddress) => {
+    for (const bond of strat["bonds"]) {
+        if (bond["bondAddress"] === bondAddress) {
+            return bond
+        }
+    }
+    return null;
 }
 
 (async () => {
