edbf48b98950745b817cd6b7fa00dd4d68709939
danielgelfand <>
2022-01-24 21:12:44 -0500

    setup for fhm changes

--- scripts/manage.js
+++ scripts/manage.js
@@ -3,15 +3,16 @@ const {bondOrStake} = require("../utils/bondOrStake.js")
 const {strats} = require('../data/strats.js');
 const {canRedeem} = require("../utils/checkRebase.js");
 
-const provider = ethers.getDefaultProvider("https://rpc.ftm.tools/");
+const provider = ethers.getDefaultProvider("https://rpc.testnet.fantom.network/");
 
 // Function to be called repeatedly to check if bond can be redeemed and
 // funds moved from staking to bonding
 const run = async () => {
 
-    let isBonding = true; // modify later to check in smart contract
+    let isBonding = false; // modify later to check in smart contract
 
     for (let i = 0; i < strats.length; i++) {
+        if (i==0) continue;
         let strat = strats[i];
         let stratContract = new ethers.Contract(strat["stratAddress"], strat["stratABI"].abi, provider);
         let is44 = strat["is44"];
@@ -19,34 +20,37 @@ const run = async () => {
         let stakingAddress = strat["stakingContract"];
         let stakingABI = strat["stakingContractABI"].abi;
 
-        let bondAddress = "0x5D449738986ab34280373502031D1513581Cb649"; // read currentBond
-        let bond = grabBondWithAddress(strat,bondAddress)
-        let bondABI = bond["bondABI"].abi;
-
         let stakingContract = new ethers.Contract(stakingAddress, stakingABI, provider);
-        let bondContract = new ethers.Contract(bondAddress, bondABI, provider);
 
-        let currBlockNum = await provider.getBlockNumber()
-        if (isBonding && await canRedeem(currBlockNum, stakingContract, bondContract, is44)) {
-            console.log("Can redeem bond!");
-            // redeem bond here
-        }
+        if (isBonding) {
+            let bondAddress = "0x5D449738986ab34280373502031D1513581Cb649"; // read currentBond
+            let bond = grabBondWithAddress(strat,bondAddress)
+            let bondABI = bond["bondABI"].abi;
+            let bondContract = new ethers.Contract(bondAddress, bondABI, provider);
+
+            let currBlockNum = await provider.getBlockNumber()
+            if (isBonding && await canRedeem(currBlockNum, stakingContract, bondContract, is44)) {
+                console.log("Can redeem bond!");
+                // redeem bond here
+            }
+            else console.log("Cant redeem bond currently", currBlockNum);
+        }   
         else {
-            console.log("Cannot redeem bond");
+            console.log("Currently not bonding");
         }
 
-        checkOptimal = await bondOrStake(strat);
+        let stakeToBond = await bondOrStake(strat);
         
-        if (checkOptimal) {
-            console.log(checkOptimal);
+        if (stakeToBond) {
+            console.log(stakeToBond);
             console.log("Checking if allowed to bond...")
             // Get SpartacusStrategy Contract and check if bonded variable is True
             // If False, stakeToBond
-            let isLP = checkOptimal["bondToken"].include("-")
+            let isLP = stakeToBond["bondToken"].include("-")
             let token0Route = bond["token0Route"];
             let token1Route = bond["token1Route"];
-            if (isLP) { stratContract.stakeToBondLPAll(bondContract, token0Route, token1Route); }
-            else { stratContract.stakeToBondSingleAll(bondContract, token0Route); }
+            // if (isLP) { stratContract.stakeToBondLPAll(bondContract, token0Route, token1Route); }
+            // else { stratContract.stakeToBondSingleAll(bondContract, token0Route); }
         }
         else {
             console.log("Continue staking...");
